f36cb8e1a11651f8442dca78b259dc55b30ad198
Ihor Matiev <ihor.matiev@gmail.com>
2025-03-21 22:36:18 +0100

    Moved tests setup to the src folder

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./test/setup.ts",
+    setupFiles: "./src/setupTests.ts",
     coverage: {
       provider: "v8"
     }

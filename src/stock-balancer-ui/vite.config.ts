f4ed9e22dbca5cd25397df5556519b3f72903070
Ihor Matiev <48079411+norison@users.noreply.github.com>
2025-03-21 22:15:49 +0100

    Feat/6 add build and test actions (#19)
    * Configured vitest and added first test
    
    * Added ci.yaml to build and test code

--- src/stock-balancer-ui/vite.config.ts
+++ src/stock-balancer-ui/vite.config.ts
@@ -1,11 +1,16 @@
 /// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "jsdom"
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test/setup.ts",
+    coverage: {
+      provider: "v8"
+    }
   }
 })

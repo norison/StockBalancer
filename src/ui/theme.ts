dfa143f3a45af15265faed661d98ba6b9b437b3c
Ihor Matiev <ihor.matiev@gmail.com>
2025-03-22 02:18:55 +0100

    Added material ui and dark theme by default

---
+++ src/ui/theme.ts
@@ -0,0 +1,13 @@
+import {createTheme} from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#ff0000',
+    },
+    secondary: {
+      main: '#00ff00',
+    }
+  }
+});
